425d49259bd15b39d9961ad37d0049f7ace8fa4f
hiroko-ino <los73.butterfly.1251.12.15@gmail.com>
2022-01-01 16:33:52 +0900

    画像の変更暫定

---
+++ server/tool/applySharpToLocal.js
@@ -0,0 +1,40 @@
+const sharp = require('sharp');
+const fs = require('fs');
+const fsPromises = fs.promises;
+const glob = require('glob');
+const path = require('path');
+
+const ORIGINAL_IMG_DIR = glob.sync('**/public/images/'); // 元画像を格納しているディレクトリ
+
+/**
+ * 画像をリサイズしてリサイズ後のディレクトリに格納する
+ * @param {string} imgPath リサイズ前の画像パス
+ * @param {string} outputFilePath リサイズ後の画像パス
+ */
+function createResizeImage(imgPath, outputFilePath) {
+  sharp(imgPath)
+  .resize({
+    fit: 'cover',
+    height: 554,
+    width: 988,
+  })
+  .toFormat('webp', { quality: 90 })
+  .toFile(outputFilePath, (err) => {
+    if ( err ) console.error(err);
+    return;
+  });
+}
+
+ORIGINAL_IMG_DIR.forEach((dirName) => {
+  const resolvedPath = path.resolve(dirName);
+  fsPromises.readdir(resolvedPath)
+    .then((files) => {
+      console.log(files);
+      files.forEach((file) => {
+        createResizeImage(resolvedPath + '/' + file, resolvedPath + '/' + file.replace('jpg', 'webp'));
+      });
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
